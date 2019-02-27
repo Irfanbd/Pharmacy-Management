568afc543f9de1e8d3cf3525d340723169bf38fd
Irfanbd <47223579+Irfanbd@users.noreply.github.com>
2019-02-27 19:55:42 +0600

    Add files via upload

---
+++ PhMS/Dashboard.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PhMS
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void exit_b(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void login_btn(object sender, RoutedEventArgs e)
+        {
+            Mainpage bw = new Mainpage();
+            if (username.Text == "irfan" && password.Password == "1234")
+            {
+                bw.Show();
+                this.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Wrong username or password", "Login");
+                username.Text = "";
+                password.Password = "";
+            }
+            
+            
+        }
+    }
+}
---
+++ PhMS/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhMS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void wxit_btn(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you want to exit?", "Exit", MessageBoxButton.YesNo);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    System.Environment.Exit(0);
+                    break;
+                case MessageBoxResult.No:
+                    MessageBox.Show("Are you sure?", "Exit");
+                    break;
+
+
+            }
+        }
+
+        private void admin_btn(object sender, RoutedEventArgs e)
+        {
+            Dashboard bw = new Dashboard();
+            bw.Show();
+        }
+
+        private void Staff_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Dashboard bw = new Dashboard();
+            bw.Show();
+        }
+    }
+}
